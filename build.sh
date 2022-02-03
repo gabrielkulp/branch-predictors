82ded1dcea4b42c70028c826ffba8b20076bab5f
gabrielkulp <kulpga@oregonstate.edu>
2022-02-02 16:27:17 -0800

    Add no-build option to build script

--- build.sh
+++ build.sh
@@ -4,48 +4,53 @@ ARCH=RISCV
 BIN_TYPE=opt
 
 usage() {
-	echo "Usage: $0 [-a ARCH] [-t TYPE] [-x] [-u REPO]"
+	echo "Usage: ${0} [-a ARCH] [-t TYPE] [-nx] [-u REPO]"
 	echo "    ARCH can be RISCV, ARM, or X86. Default is RISCV."
 	echo "    TYPE can be debug, opt, fast, prof, perf. Default is opt."
+	echo "    -n to skip building (you could use this with -x and -u)"
 	echo "    -x to create a smaller export container at the end."
 	echo "    -u to upload the final exported container to REPO/gem5:ARCH."
 	exit 2
 }
 
-while getopts 'xa:t:u:' c
+while getopts 'xna:t:u:' c
 do
-case $c in
+case ${c} in
 	x) EXPORT=true ;;
-	a) ARCH="$OPTARG" ;;
-	t) BIN_TYPE="$OPTARG" ;;
-	u) OWNER="$OPTARG" ;;
+	n) NOBUILD=true ;;
+	a) ARCH="${OPTARG}" ;;
+	t) BIN_TYPE="${OPTARG}" ;;
+	u) OWNER="${OPTARG}" ;;
 	*) usage ;;
 esac; done
 
 # build the full development image (it's over 5GB)
-echo "Building $ARCH/gem5.$BIN_TYPE executable"
-docker build . \
-	--build-arg "ARCH=$ARCH" \
-	--build-arg "BIN_TYPE=$BIN_TYPE" \
-	-t gem5-dev:"$ARCH"
+# but only if -n is NOT provided
+if [ -z "${NOBUILD}" ]; then
+	echo "Building ${ARCH}/gem5.${BIN_TYPE} executable"
+	docker build . \
+		--build-arg "ARCH=${ARCH}" \
+		--build-arg "BIN_TYPE=${BIN_TYPE}" \
+		-t gem5-dev:"${ARCH}"
+fi
 
 # only do the export if -x is provided
-if [ -n "$EXPORT" ]; then
+if [ -n "${EXPORT}" ]; then
 	echo "Creating new container to export"
-	IMAGE=$(docker build -q --build-arg "ARCH=$ARCH" - < Dockerfile-release)
-	ID=$(docker create "$IMAGE")
+	IMAGE=$(docker build -q --build-arg "ARCH=${ARCH}" - < Dockerfile-release)
+	ID=$(docker create "${IMAGE}")
 	echo "Exporting executable to smaller image"
-	docker export "$ID" | docker import - "gem5:$ARCH" -c 'WORKDIR /opt' -c 'CMD [ "gem5", "--help" ]'
-	echo "Tagged gem5:$ARCH"
+	docker export "${ID}" | docker import - "gem5:${ARCH}" -c 'WORKDIR /opt' -c 'CMD [ "gem5", "--help" ]'
+	echo "Tagged gem5:${ARCH}"
 	echo "Removing temporary container and image"
-	docker rm "$ID"
-	docker rmi "$IMAGE"
+	docker rm "${ID}"
+	docker rmi "${IMAGE}"
 fi
 
 # only do the upload if -u is provided
-if [ -n "$OWNER" ]; then
-	docker tag gem5:"$ARCH" "$OWNER/gem5:$ARCH"
-	docker push "$OWNER/gem5:$ARCH"
+if [ -n "${OWNER}" ]; then
+	docker tag "gem5:${ARCH}" "${OWNER}/gem5:${ARCH}"
+	docker push "${OWNER}/gem5:${ARCH}"
 fi
 
 echo "Done!"
